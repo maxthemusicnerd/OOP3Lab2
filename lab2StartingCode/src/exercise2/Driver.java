5420c2c6193c3b6321f9a421e31c1bf2d25a1599
maximL5 <maxim.lopez@edu.sait.ca>
2025-02-21 20:40:13 -0700

    Exercise 2 code

--- lab2StartingCode/src/exercise2/Driver.java
+++ lab2StartingCode/src/exercise2/Driver.java
@@ -1,12 +1,15 @@
 package exercise2;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Driver
 {
 
 	public static final int SIZE = 100;
 	public static final int UPPER_BOUND = 10;
+	
+	
 
 	public static void main( String[] args )
 	{
@@ -22,7 +25,42 @@ public class Driver
 			nums[i] = nums[i - 1] + randnum;
 			System.out.println( nums[i] );
 		}
+		
+		Scanner scanner = new Scanner(System.in); 
+	    System.out.println("Enter number: ");
 
+	    int userNum = scanner.nextInt();  
+	    scanner.close();
+	    
+	    int index = binarySearch(nums, userNum); 
+	    
+	    if (index != -1) {
+	        System.out.println(index);
+	    } else {
+	        System.out.println("-1");
+	    }
+	    
+	    
+
+	}
+	
+	public static int binarySearch(Integer[] nums, int target) {
+		int left = 0;
+	    int right = nums.length - 1;
+
+	    while (left <= right) {  
+	        int mid = (left + right) / 2; 
+
+	        if (nums[mid] == target) {  
+	            return mid;  
+	        } else if (nums[mid] < target) {  
+	            left = mid + 1; 
+	        } else {  
+	            right = mid - 1;  
+	        }
+	    }
+
+	    return -1;  
 	}
 
 }
